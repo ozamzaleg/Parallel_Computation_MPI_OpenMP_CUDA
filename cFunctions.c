dd8e4476477185cd04706765ee47e6ac87c45fe2
Oz Amzaleg <73498422+ozamzaleg@users.noreply.github.com>
2022-05-30 18:34:15 +0300

    Update cFunctions.c

--- cFunctions.c
+++ cFunctions.c
@@ -4,9 +4,8 @@
 
 int test(int *data, int n)
 {
-	int i;
 	int counter = 0;
-	for (i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		if (f(data[i]) > 0)
 		{
@@ -19,11 +18,10 @@ int test(int *data, int n)
 }
 double f(int i)
 {
-	int j;
 	double value;
 	double result = 0;
 
-	for (j = 1; j < HEAVY; j++)
+	for (int j = 1; j < HEAVY; j++)
 	{
 		value = (i + 1) * (j % 10);
 		result += cos(value);
